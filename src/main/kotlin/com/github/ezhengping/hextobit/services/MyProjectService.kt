9de09327da77931a83dc0620a3671c48ed4066b2
GitHub Action <action@github.com>
2023-04-15 23:49:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/ezhengping/hextobit/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ezhengping/hextobit/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ezhengping.hextobit.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ezhengping.hextobit.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
